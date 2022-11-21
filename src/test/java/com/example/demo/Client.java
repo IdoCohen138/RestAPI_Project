0964f2ff3a02fc3899c2702bd089ccb8bdef584c
liorkobi <liorkobi2021@gmailc.com>
2022-11-21 20:26:12 +0200

    myClient test-first try integrationTest-first try

--- src/test/java/com/example/demo/Client.java
+++ src/test/java/com/example/demo/Client.java
@@ -1,6 +1,10 @@
 package com.example.demo;
 
-import controllers.SlackChannelController;
+import com.example.demo.controllers.SlackChannelController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.Map;
 
 public class Client {
     private SlackChannelController slackChannelController;
@@ -9,9 +13,38 @@ public class Client {
         this.slackChannelController = slackChannelController;
     }
 
-    public boolean CreateChannel(String channelName, String webhook){
+    public boolean CreateChannelTest(Map<String, String> json) {
+        ResponseEntity<String> return_ = new ResponseEntity<String>("The channel has been created successful.", HttpStatus.OK);
+        if (return_.equals(slackChannelController.createChannel(json))) {
+            return true;
+        }
 
-    return slackChannelController.CreateChannel(channelName,webhook);
-}
+        return false;
+    }
+    public boolean updateChannelTest(Map<String, String> json) {
+        ResponseEntity<String> return_ = new ResponseEntity<String>("TThe channel status has been modify successful.", HttpStatus.OK);
+        if (return_.equals(slackChannelController.updateChannel(json))) {
+            return true;
+        }
+
+        return false;
+    }
+    public boolean deleteChannelTest(Map<String, String> json) {
+        ResponseEntity<String> return_ = new ResponseEntity<String>("The channel has been deleted successful.", HttpStatus.OK);
+        if (return_.equals(slackChannelController.deleteChannel(json))) {
+            return true;
+        }
+
+        return false;
+    }
+    public boolean getSpecificChannelTest(Map<String, String> json) {
+        ResponseEntity<String> return_ = new ResponseEntity<String>("There is no channel with this details.", HttpStatus.OK);
+        if (return_.equals(slackChannelController.createChannel(json))) {
+            return false;
+        }
+
+        return true;
+    }
+
 
 }
