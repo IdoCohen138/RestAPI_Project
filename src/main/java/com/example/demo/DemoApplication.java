18045c0e8e25e6c569e86dd76fa65836e07ba51d
Shahaf15 <poursh@post.bgu.ac.il>
2022-11-30 16:56:29 +0200

    Slack Integration

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,10 +1,8 @@
 package com.example.demo;
 
+import com.example.demo.serviceLayer.SlackIntegration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 
--- src/main/java/com/example/demo/serviceLayer/SlackChannelController.java
+++ src/main/java/com/example/demo/serviceLayer/SlackChannelController.java
@@ -4,20 +4,31 @@ import com.example.demo.presentationLayer.Exceptions.ChannelAlreadyExitsInDataBa
 import com.example.demo.presentationLayer.Exceptions.ChannelNotExitsInDataBaseException;
 import com.example.demo.presentationLayer.dataBase;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class SlackChannelController implements channelRepository{
     repository dataBaseInterface = new dataBase();
-
+    private SlackIntegration si = new SlackIntegration();
     @Override
     public void createChannel(SlackChannel slackChannel) throws ChannelAlreadyExitsInDataBaseException {
         dataBaseInterface.createChannel(slackChannel);
+        try{
+            si.sendPeriodicMessage(slackChannel);
+        } catch (IOException e) {
+            System.out.println("Cant send Slack Message");
+        }
     }
 
     @Override
     public void updateChannel(SlackChannel slackChannel) throws ChannelNotExitsInDataBaseException {
         SlackChannel modifyChannel = dataBaseInterface.updateChannel(slackChannel);
         modifyChannel.setStatus();
+        try{
+            this.si.sendStatusMessage(slackChannel);
+        } catch (IOException e) {
+            System.out.println("Message cant sent to Slack");
+        }
     }
 
     @Override
