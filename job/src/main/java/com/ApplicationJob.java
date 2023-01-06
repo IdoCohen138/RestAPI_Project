9fea8b0c8cca371811a4fd3eb6f9795570b7d8e8
IdoCohen138 <ido3@post.bgu.ac.il>
2023-01-06 11:54:29 +0200

    ido Push - fix LogMessage @JsonIgnore

--- job/src/main/java/com/ApplicationJob.java
+++ job/src/main/java/com/ApplicationJob.java
@@ -1,21 +1,10 @@
 package com;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-
-@SpringBootApplication
-@EntityScan(basePackageClasses = {com.LogMessagePrimaryKey.class, com.LogMessages.class, com.SlackChannel.class})
-@EnableJpaRepositories
 public class ApplicationJob {
 
     public static void main(String[] args) {
-        SpringApplication.run(ApplicationJob.class, args);
-        ConfigurableApplicationContext context = SpringApplication.run(ApplicationJob.class, args);
-        SlackIntegration slackIntegration = context.getBean(SlackIntegration.class);
+        SlackIntegration slackIntegration= new SlackIntegration();
         slackIntegration.sendPeriodicMessages();
     }
 }
