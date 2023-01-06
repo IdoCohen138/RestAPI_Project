e23cfb6db3fb457813123cd475b34f9b20de0788
IdoCohen138 <ido3@post.bgu.ac.il>
2023-01-06 10:04:15 +0200

    ido Push - rebuild structure and add job_pom add pom for each module etc..

--- src/main/java/com/application/persistence/exceptions/ChannelNotExitsInDataBaseException.java
+++
@@ -1,7 +0,0 @@
-package com.application.persistence.exceptions;
-
-public class ChannelNotExitsInDataBaseException extends Exception {
-    public ChannelNotExitsInDataBaseException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/application/service/EnumStatus.java
+++
@@ -1,7 +0,0 @@
-package com.application.service;
-
-public enum EnumStatus {
-    ENABLED, DISABLED
-
-
-}
