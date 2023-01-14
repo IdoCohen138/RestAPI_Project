ba8ddbedb950c43ea0f38b12b022586821175bb1
IdoCohen138 <81559737+IdoCohen138@users.noreply.github.com>
2023-01-14 14:58:45 +0200

    Add files via upload

--- SCRIPT/Script_Test-Modules/TestModules.py
+++ SCRIPT/Script_Test-Modules/TestModules.py
@@ -7,13 +7,18 @@ path_to_your_location="C:/Users/Ido/PycharmProjects/SCRIPT/Script_Test-Modules"
 os.system("git clone https://github.com/IdoCohen138/RestAPI_Project.git")
 
 # Navigate to the persistence module
-os.chdir(path_to_your_location+"/RestAPI_Project/persistence")
+os.chdir(path_to_your_location+"/RestAPI_Project")
 
-# Copy the config.properties file
-shutil.copy2(path_to_your_location + "/config.properties", "src/test/resources/")
+# Copy the config.properties file to persistence module
+shutil.copy2(path_to_your_location + "/config.properties", "persistence/src/test/resources/")
+
+os.mkdir(path_to_your_location + "/RestAPI_Project/job/src/test/resources")
+
+# Copy the config.properties file to job module
+shutil.copy2(path_to_your_location + "/config.properties", "job/src/test/resources/")
 
 # Copy the .env file
 shutil.copy2(path_to_your_location + "/.env", path_to_your_location+"/RestAPI_Project")
 
 # Package the module using Maven
-os.system("mvn package")
+os.system("mvn package -f job_pom.xml")
